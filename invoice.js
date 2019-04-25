134332cedf3484491a3771a114faca3df622ee1c
Shivdhwaj Pandey <shivdhwaj@gmail.com>
2019-04-25 12:28:53 +0530

    Initial Commit

---
+++ invoice.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [{
+    'index': 1,
+    'text': 'Hello Developer X, You are charged $350 this month for services.'
+    },{
+    'index': 2,
+    'text': 'Hello Developer Y, You are charged $60 this month for services.'
+    },{
+    'index': 3,
+    'text': 'Hello Developer Z, You are charged $99 this month for services.'
+    }]
+    }
